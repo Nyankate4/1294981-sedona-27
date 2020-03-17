fe16c784b600e532468f3f5fdd2c34c456407791
Nyankate4 <60111121+Nyankate4@users.noreply.github.com>
2020-03-17 19:40:06 +0300

    Завершаю проект

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var link = document.querySelector(".popup-button");
+var popup = document.querySelector(".dialog");
+popup.classList.remove('modal-show');
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (popup.classList.contains('modal-show')) {
+    popup.classList.remove('modal-show');
+  } else {
+    popup.classList.add('modal-show');
+  }
+});
